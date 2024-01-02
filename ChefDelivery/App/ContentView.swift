35571df62932b7eaeff361bc8fa5a77cc8ad07ba
Quellenni Reis <quellenni.barreto@g.globo>
2024-01-02 15:20:36 -0300

    Implements Async methods

--- ChefDelivery/App/ContentView.swift
+++ ChefDelivery/App/ContentView.swift
@@ -12,31 +12,56 @@ struct ContentView: View {
     //MARK: - Atributes
     
     private var service = HomeService()
+    @State private var storesType: [StoreType] = []
+    
+    @State private var isLoading = true
     
     //MARK: - View
     var body: some View {
         NavigationView {
             VStack(spacing: 0) {
-                NavigationBarView()
-                    .padding(.horizontal, 15)
-                ScrollView(.vertical, showsIndicators: false) {
-                    VStack(spacing: 20) {
-                        OrderTypeGridView()
-                        CarouselTabView()
-                        StoresContainerView()
+                if isLoading {
+                    ProgressView()
+                } else {
+                    NavigationBarView()
+                        .padding(.horizontal, 15)
+                    
+                    ScrollView(.vertical, showsIndicators: false) {
+                        VStack(spacing: 20) {
+                            OrderTypeGridView()
+                            CarouselTabView()
+                            StoresContainerView(stores: storesType)
+                        }
                     }
                 }
-               
             }
         }
         .onAppear {
-            service.fetchData()
+            Task {
+                await getStores()
+            }
         }
     }
     
     // MARK: - Methods
     
-   
+    func getStores() async {
+        do {
+            let result = try await service.fetchData()
+            switch result {
+            case.success(let stores):
+                self.storesType = stores
+                self.isLoading = false
+            case.failure(let error):
+                print(error.localizedDescription)
+                self.isLoading = false
+            }
+        } catch {
+            print(error.localizedDescription)
+            self.isLoading = false
+        }
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
